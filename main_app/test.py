debad47f6adf805006267a447399e5324eea8a2d
rollotea <135215892+rollotea@users.noreply.github.com>
2024-01-27 19:22:22 +0900

    Update test.py

--- main_app/test.py
+++ main_app/test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Initialize connection.
-conn = st.connection('mysql', url="mysql://root:dxftk286@localhost:3306/pets")
+conn = st.connection('mysql', type="sql", url="mysql://root:dxftk286@localhost:3306/pets")
 
 # Perform query.
 df = conn.query('SELECT * from mytable;', ttl=600)
