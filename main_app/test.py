8bc63b6d6183ed333983b015b8c185be82b89bc9
rollotea <135215892+rollotea@users.noreply.github.com>
2024-01-27 19:03:53 +0900

    Update test.py

--- main_app/test.py
+++ main_app/test.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import sqlalchemy as sa
 
-engine = sa.create_engine("mysql+pymysql://root:dxftk286@localhost:3306/pets")
+engine = sa.create_engine("mysql://root:dxftk286@localhost:3306/pets")
 df = pd.read_sql_query(sql="SELECT * FROM mytable", con=engine)
 st.dataframe(df)
